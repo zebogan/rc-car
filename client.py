5d0ba5ea664fac90cd1796f754fbe88a6db224d6
zebogan <scienceguylrs@gmail.com>
2024-10-10 11:42:46 -0700

    did stuff

--- client.py
+++ client.py
@@ -1,22 +1,49 @@
 import socket, cv2, numpy, time
 
-#TCP_IP = '10.240.32.228'
-TCP_IP = '10.0.0.22'
+TCP_IP = '10.240.33.129'
+#TCP_IP = '10.0.0.22'
 TCP_PORT = 10001
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 160000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(("0.0.0.0", 10002))
+server.listen()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((TCP_IP,TCP_PORT))
 
+conn, addr = server.accept()
+print("setup done")
+
 while True:
     d = b''
-    while len(d) < 1228800:
-        data = sock.recv(BUFFER_SIZE)
-        d += data
+    
+    sock.settimeout(0.1)
+
+    while True:
+        try:
+            print("about to get data")
+            data = sock.recv(BUFFER_SIZE)
+            print("data recvd")
+            d += data
+            print(len(d))
+            if len(d) == 921600:
+                break
+        except:
+            print("breaking")
+            break
+    
+    conn.sendall("RECIEVED".encode())
+
+    print("data done")
 
     b = bytearray(d)
-    arr = numpy.array(b).reshape(480, 640, 4)
+    print(len(b))
+    arr = numpy.array(b).reshape(480, 640, 3)
     cv2.imshow("name", arr)
+    cv2.waitKey(10)
 
 cv2.destroyAllWindows()
 sock.close()
+conn.close()
--- server.py
+++ server.py
@@ -4,21 +4,43 @@ import socket
 import time
 
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
-from picamera2.outputs import FileOutput
+
+#TCP_IP = '10.0.0.102'
+TCP_IP = '10.240.35.22'
+#TCP_IP = '10.240.33.129'
+TCP_PORT = 10002
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 picam2 = Picamera2()
+
+config = picam2.create_preview_configuration()
+config['main']['format'] = 'RGB888'
+print(config)
+picam2.configure(config)
+
+picam2.start()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind(("0.0.0.0", 10001))
     sock.listen()
 
+    print("waiting for connection")
     conn, addr = sock.accept()
-    picam2.start()
+
+    client.connect((TCP_IP, TCP_PORT))
+
+    print("setup done")
+
     while True:
-        frame = picam2.capture_array()
+        frame = picam2.capture_array()  
+        print("took pic")
+        conn.sendall(frame.tobytes())
         print(len(frame.tobytes()))
-        conn.send(frame.tobytes())
+        print("img sent")
+        client.recv(4096)
+        
     picam2.stop()
     conn.close()
+    client.close()
