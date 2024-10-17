0d18800add11d3336ab809a93a2695eb0e822c8d
zebogan <scienceguylrs@gmail.com>
2024-10-17 11:20:44 -0700

    totally redid everything

--- server.py
+++ server.py
@@ -1,46 +1,73 @@
 #!/usr/bin/python3
 
-import socket
-import time
+# Mostly copied from https://picamera.readthedocs.io/en/release-1.13/recipes2.html
+# Run this script, then point a web browser at http:<this-ip-address>:8000
+# Note: needs simplejpeg to be installed (pip3 install simplejpeg).
+
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
 
 from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 
-#TCP_IP = '10.0.0.102'
-TCP_IP = '10.240.35.22'
-#TCP_IP = '10.240.33.129'
-TCP_PORT = 10002
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
 
 picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (1200, 800)}))
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
 
-config = picam2.create_preview_configuration()
-config['main']['format'] = 'RGB888'
-print(config)
-picam2.configure(config)
-
-picam2.start()
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("0.0.0.0", 10001))
-    sock.listen()
-
-    print("waiting for connection")
-    conn, addr = sock.accept()
-
-    client.connect((TCP_IP, TCP_PORT))
-
-    print("setup done")
-
-    while True:
-        frame = picam2.capture_array()  
-        print("took pic")
-        conn.sendall(frame.tobytes())
-        print(len(frame.tobytes()))
-        print("img sent")
-        client.recv(4096)
-        
-    picam2.stop()
-    conn.close()
-    client.close()
+try:
+    address = ('', 8000)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
