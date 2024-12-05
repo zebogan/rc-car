249e36a7eab21086117ad9a17e033da96c63199e
zebogan <scienceguylrs@gmail.com>
2024-12-05 10:17:33 -0800

    added nocam

---
+++ server-nocam.py
@@ -0,0 +1,82 @@
+import io
+import logging
+import socketserver
+from http import server
+from threading import Condition
+import motor
+import json
+
+# from picamera2 import Picamera2
+# from picamera2.encoders import JpegEncoder
+# from picamera2.outputs import FileOutput
+
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/stream.mjpg':
+            self.send_response(200)
+            self.send_header('Age', 0)
+            self.send_header('Cache-Control', 'no-cache, private')
+            self.send_header('Pragma', 'no-cache')
+            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.end_headers()
+            try:
+                while True:
+                    with output.condition:
+                        output.condition.wait()
+                        frame = output.frame
+                    self.wfile.write(b'--FRAME\r\n')
+                    self.send_header('Content-Type', 'image/jpeg')
+                    self.send_header('Content-Length', len(frame))
+                    self.end_headers()
+                    self.wfile.write(frame)
+                    self.wfile.write(b'\r\n')
+            except Exception as e:
+                logging.warning(
+                    'Removed streaming client %s: %s',
+                    self.client_address, str(e))
+        else:
+            self.send_error(404)
+            self.end_headers()
+    def do_POST(self):
+        content_len = int(self.headers.get('Content-Length'))
+        post_body = self.rfile.read(content_len)
+        j = json.loads(post_body)
+        if j['direction'] == 'f':
+            motor.forward()
+        if j['direction'] == 'b':
+            motor.back()
+        if j['direction'] == 'r':
+            motor.right()
+        if j['direction'] == 'l':
+            motor.left()
+    
+
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+    allow_reuse_address = True
+    daemon_threads = True
+
+
+# picam2 = Picamera2()
+# picam2.configure(picam2.create_video_configuration(main={"size": (1200, 800)}))
+# output = StreamingOutput()
+# picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+try:
+    address = ('', 10001)
+    server = StreamingServer(address, StreamingHandler)
+    server.serve_forever()
+finally:
+    picam2.stop_recording()
